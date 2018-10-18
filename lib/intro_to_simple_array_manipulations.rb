06a80eb4062b329b98a08baf6c808993151158db
Leslie Osorio <lesosorio17@gmail.com>
2018-10-18 03:06:36 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,9 +6,10 @@ def using_unshift(array,string)
   array.unshift(string)
 end
 
-def using_pop(array,string)
-  array.pop
+def using_pop(fav_foods)
+  fav_foods = ["chicken strips", "waffles", "french fries"]
+  fav_foods.pop
 end
 
 def pop_with_args(array,string)
-  array.pop(2)
+  array.pop
